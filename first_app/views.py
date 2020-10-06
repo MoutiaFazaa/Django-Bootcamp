251d56499b012587e0fd9670966a54e7266019f5
MoutiaFazaa <fazaamouty@gmail.com>
2020-10-06 00:31:29 -0400

    MTV model done with success

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import topic,Webpage,AccessRecord
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "hello i am from first_app views.py"}
-    return render(request,'first_app/index.html',context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    return render(request,'first_app/index.html',context=date_dict)
